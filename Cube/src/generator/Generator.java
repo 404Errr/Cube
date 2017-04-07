cd866bf8a1637c3e77fc209d2c8e7620ca138fe5
404Errr <auggieam@gmail.com>
2017-04-07 13:44:48 -0400

    Update Generator.java

--- Cube/src/generator/Generator.java
+++ Cube/src/generator/Generator.java
@@ -1,13 +1,81 @@
 package generator;
 
 public class Generator {
-
+	
 
 
 
 
 
 }
+
+class Pointer implements GeneratorData {
+	
+	
+	private int x, y, z;
+	
+	public Pointer(int x, int y, int z) {
+		set(x, y, z);
+	}
+	
+	public Pointer getOffset(int direction) {
+		switch (direction) {
+		case PX:
+			return new Pointer(x+1, y, z);
+		case PY:
+			return new Pointer(x, y+1, z);
+		case PZ:
+			return new Pointer(x, y, z+1);
+		case NX:
+			return new Pointer(x-1, y, z);
+		case NY:
+			return new Pointer(x, y-1, z);
+		case NZ:
+			return new Pointer(x, y, z-1);
+		}
+	}
+	
+	public void set(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
+					
+class Cube {
+	
+	
+	private int[][][] cubies;
+	private boolean[][][] added;
+	
+	public Cube() {
+		
+	}
+
+
+
+
+
+}
+
+
+
+
+
+interface GeneratorData {
+	int SIZE = 3;
+	
+	int PX = 0, PY = 1, PZ = 2, NX = 3, NY = 4, NZ = 5;
+}
+
+
+
+
+
+
+
+
+
 
 
 
