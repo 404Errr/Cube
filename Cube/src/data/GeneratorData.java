7059288a94d5481038b328e71f017dd8d3fb9e4a
404 <auggieam@gmail.com>
2017-04-27 02:45:18 -0400

    geges

--- Cube/src/data/GeneratorData.java
+++ Cube/src/data/GeneratorData.java
@@ -7,8 +7,10 @@ public interface GeneratorData {
 	boolean SAVE = true;
 	String PATH = "src/cubes";
 
-	float PIECE_COUNT_RATIO = (Size.SIZE==3)?0.2f:0.15f;
-	int PIECE_SIZE_MIN_OFFSET = (Size.SIZE==3)?0:1, PIECE_SIZE_MAX_OFFSET = (Size.SIZE==3)?0:1;
+	float PIECE_COUNT_RATIO = 0.2f;
+	int PIECE_SIZE_MIN_OFFSET = 0, PIECE_SIZE_MAX_OFFSET = 0;
+//	float PIECE_COUNT_RATIO = 0.15f;
+//	int PIECE_SIZE_MIN_OFFSET = 1, PIECE_SIZE_MAX_OFFSET = 1;
 
 	int PIECE_COUNT = (int) (MainData.SIZE*MainData.SIZE*MainData.SIZE*PIECE_COUNT_RATIO);
 	int PIECE_SIZE_MIN = (int) (Math.floor((MainData.SIZE*MainData.SIZE*MainData.SIZE)/(float) PIECE_COUNT)-PIECE_SIZE_MIN_OFFSET);
@@ -21,16 +23,10 @@ public interface GeneratorData {
 
 	boolean CHECK_PRINT = false;
 
-//	boolean CHECK_FLAT = true;
-//	boolean CHECK_TOO_MANY_ON_PLANE = true;
-//	boolean CHECK_COLLISION = true;
-//	boolean CHECK_2D_CLUSTERS = false;
-//	boolean CHECK_3D_CLUSTERS = true;
-//	boolean CHECK_IDENTICAL = true;
-
 	boolean CHECK_FLAT = true;
 	boolean CHECK_TOO_MANY_ON_PLANE = true;
 	boolean CHECK_COLLISION = true;
+//	boolean CHECK_2D_CLUSTERS = false;
 	boolean CHECK_2D_CLUSTERS = true;
 	boolean CHECK_3D_CLUSTERS = true;
 	boolean CHECK_IDENTICAL = true;
